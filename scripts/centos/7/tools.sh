02823d636224ba58db0f7fbd537a676e9a7a3172
Eric Shamow <eric.shamow@standard.com>
2015-11-16 15:22:05 -0800

    Working puppet

--- scripts/centos/7/tools.sh
+++ scripts/centos/7/tools.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export http_proxy='http://proxy.standard.com:8080'
+export https_proxy='http://proxy.standard.com:8080'
+export HTTP_PROXY='http://proxy.standard.com:8080'
+export HTTPS_PROXY='http://proxy.standard.com:8080'
 yum -y install git
--- scripts/common/proxy.sh
+++ scripts/common/proxy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "export http_proxy='http://proxy.standard.com:8080/'" >> /etc/profile
-echo "export HTTP_PROXY=\"$http_proxy\"" >> /etc/profile
-echo "export https_proxy='http://proxy.standard.com:8080/'" >> /etc/profile
-echo "export HTTPS_PROXY=\"$http_proxy\"" >> /etc/profile
-
+echo "export http_proxy='http://proxy.standard.com:8080'" >> /etc/environment
+echo "export HTTP_PROXY='http://proxy.standard.com:8080'" >> /etc/environment
+echo "export https_proxy='http://proxy.standard.com:8080'" >> /etc/environment
+echo "export HTTPS_PROXY='http://proxy.standard.com:8080'" >> /etc/environment
