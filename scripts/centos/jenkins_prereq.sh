0a60d060f9ebe40edd07000a6ee829795a6b7d92
Eric Shamow <eric.shamow@standard.com>
2015-11-17 12:55:16 -0800

    Working Jenkins box

--- scripts/centos/jenkins_prereq.sh
+++ scripts/centos/jenkins_prereq.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export http_proxy=http://proxy.standard.com:8080
+export https_proxy=http://proxy.standard.com:8080
+export HTTP_PROXY=http://proxy.standard.com:8080
+export HTTPS_PROXY=http://proxy.standard.com:8080
 puppet module install rtyler-jenkins
 puppet module install puppetlabs-firewall
 puppet module install puppetlabs-java
