fd8eff9e55c948cd5cac7bc2e96ffcba861ff029
Eric Shamow <eric.shamow@standard.com>
2015-11-17 13:20:18 -0800

    Finish packer conversion

--- scripts/centos/7/puppet4.sh
+++ scripts/centos/7/puppet4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export http_proxy=http://proxy.standard.com:8080
+export https_proxy=http://proxy.standard.com:8080
+export HTTP_PROXY=http://proxy.standard.com:8080
+export HTTPS_PROXY=http://proxy.standard.com:8080
+
 yum clean all
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent
