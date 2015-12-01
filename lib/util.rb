72b4294de529909d5da4805edb003762bdbd7fbd
Eric Shamow <eric.shamow@standard.com>
2015-12-01 11:59:13 -0800

    Rubocop fixes

--- lib/util.rb
+++ lib/util.rb
@@ -1,15 +1,13 @@
 def stream_output(cmd)
-  begin
-    PTY.spawn(cmd) do |stdout, stdin, pid|
-      begin
-        # Do stuff with the output here. Just printing to show it works
-        stdout.each { |line| print line }
-      rescue Errno::EIO
-        puts "Errno:EIO error, but this probably just means " +
-              "that the process has finished giving output"
-      end
+  PTY.spawn(cmd) do |stdout, _stdin, _pid|
+    begin
+      # Do stuff with the output here. Just printing to show it works
+      stdout.each { |line| print line }
+    rescue Errno::EIO
+      puts 'Errno:EIO error, but this probably just means ' \
+           'that the process has finished giving output'
     end
-  rescue PTY::ChildExited
-    puts "The child process exited!"
   end
+rescue PTY::ChildExited
+  puts 'The child process exited!'
 end
