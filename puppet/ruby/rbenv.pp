21ae8c377fadf3285f989852a946969b7dd9254a
Eric Shamow <eric.shamow@standard.com>
2015-12-23 10:51:58 -0800

    Get centos-6-puppet-rbenv working

--- puppet/ruby/rbenv.pp
+++ puppet/ruby/rbenv.pp
@@ -1,5 +1,9 @@
 include stdlib
 
+Rbenv::Gem {
+  ruby_version => '2.2.2',
+}
 class { '::rbenv': }
 rbenv::plugin { 'sstephenson/ruby-build': latest => true }
-rbenv::build { '2.2.3': global => true }
+rbenv::build { '2.2.2': global => true }
+rbenv::gem { ['rake','serverspec']: }
