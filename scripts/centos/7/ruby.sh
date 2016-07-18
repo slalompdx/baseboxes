85f6263d6c0f8907ad637316ca4d213392df0476
Terri Haber <terri.haber@standard.com>
2016-07-18 09:55:48 -0700

    still not in shape for merging

--- scripts/centos/7/ruby.sh
+++ scripts/centos/7/ruby.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-#curl -s https://packagecloud.io/install/repositories/slalompdx/ruby/script.rpm.sh | sudo bash
-#sudo yum -y install ruby2-2.3.1-1.el7.centos.x86_64
+cat > /etc/yum.repos.d/slalompdx_ruby.repo <<EOH
+[slalompdx_ruby]
+name=slalompdx_ruby
+baseurl=https://packagecloud.io/slalompdx/ruby/el/7/\$basearch
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/slalompdx/ruby/gpgkey
+metadata_expire=300
 
-curl http://www.yahoo.com
+[slalompdx_ruby-source]
+name=slalompdx_ruby-source
+baseurl=https://packagecloud.io/slalompdx/ruby/el/7/SRPMS
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/slalompdx/ruby/gpgkey
+metadata_expire=300
+EOH
+
+yum clean all
+
+sudo yum -y install ruby2-2.3.1-1.el7.centos.x86_64
