c8501a510f73a0bc20671b410263387dfc526948
Terri Haber <terri.haber@standard.com>
2016-08-16 13:49:24 -0700

    need a short sleep in install puppet powershell script to give puppet time to install

--- scripts/windows/install_puppet.ps1
+++ scripts/windows/install_puppet.ps1
@@ -4,4 +4,7 @@ Write-Output "Downloading $puppet_download_url"
 (New-Object System.Net.WebClient).DownloadFile($puppet_download_url, "C:\Windows\Temp\puppet-agent-1.3.6-x64.msi")
 
 Write-Output "Installing puppet..."
-msiexec /qn /norestart /i C:\Windows\Temp\puppet-agent-1.3.6-x64.msi
+msiexec /qn /norestart /i C:\Windows\Temp\puppet-agent-1.3.6-x64.msi /Lv C:\Windows\Temp\Log.txt
+
+# give puppet some time to install
+Start-Sleep -s 60
