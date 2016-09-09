2e71f6eb31dc098003f37e3c30b100875e0b98eb
Eric Shamow <alice@example.com>
2016-09-09 14:46:43 -0700

    Enable cli flags for builder and format type

--- lib/tasks/build.rake
+++ lib/tasks/build.rake
@@ -1,6 +1,7 @@
 desc 'Build specified image'
 task :build do
   name = ENV['BASE_BUILD'] || nil
+  builder = ( ENV['BUILDER'] || 'vmware,virtualbox' ).split(',')
   abort 'Set BASE_BUILD to specify a target' unless name
   puts "Building image #{name}"
   if name =~ /^\w*-\w*$/
@@ -18,7 +19,9 @@ task :build do
       "packer-#{name}-vmware/packer-vmware-vmx.vmxf"
     stream_output command
   else
-    stream_output build_packer_command(format: 'ovf', box: name)
+    format = ENV['FORMAT'] || 'ovf'
+    puts build_packer_command(builder: builder, format: format, box: name)
+    stream_output build_packer_command(builder: builder, format: format, box: name)
     command = "mv packer-#{name}-vmware/packer-vmware-vmx-{{timestamp}}.vmsd " \
       "packer-#{name}-vmware/packer-vmware-vmx.vmsd"
     stream_output command
--- lib/util/packer.rb
+++ lib/util/packer.rb
@@ -1,6 +1,7 @@
 # rubocop:disable Metrics/MethodLength
 def build_packer_command(args = {})
   defaults = {
+    builder: ['vmware', 'virtualbox'],
     format: 'iso',
     box: 'centos-7.1-x86_64',
     force: ENV['BOX_FORCE'] || false
@@ -11,17 +12,27 @@ def build_packer_command(args = {})
 
   actual = defaults.merge(args)
 
-  command = ['packer build']
-  command << '--force' if actual[:force]
-  command << "-var http_proxy=#{http_proxy}" if http_proxy
-  command << "-var https_proxy=#{https_proxy}" if https_proxy
-  command << "-var no_proxy=#{no_proxy}" if no_proxy
-  if actual[:format] == 'iso'
-    command << "-only=virtualbox-#{actual[:format]},vmware-#{actual[:format]}"
-  else
-    command << "-only=virtualbox-ovf,vmware-vmx"
+  command = 'packer build'
+  command << ' --force' if actual[:force]
+  command << " -var http_proxy=#{http_proxy}" if http_proxy
+  command << " -var https_proxy=#{https_proxy}" if https_proxy
+  command << " -var no_proxy=#{no_proxy}" if no_proxy
+  command << " -only="
+  actual[:builder].each_with_index do |builder, index|
+    if actual[:format] == 'ovf'
+      if builder == 'vmware'
+        format = 'vmx'
+      else
+        format = 'ovf'
+      end
+    else
+      format = actual[:format]
+    end
+    command << "#{builder}-#{format}"
+    unless index == actual[:builder].size - 1
+      command << ","
+    end
   end
-  command << "#{actual[:box]}.json"
-  command.join(' ')
+  command << " #{actual[:box]}.json"
 end
 # rubocop:enable Metrics/MethodLength
