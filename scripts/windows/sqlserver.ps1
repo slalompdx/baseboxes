b45b23b1cc842dae392436a0b345994ee33d0052
Eric Shamow <alice@example.com>
2016-09-28 09:49:15 -0700

    Move file transfer to packer

--- scripts/windows/sqlserver.ps1
+++ scripts/windows/sqlserver.ps1
@@ -1,7 +1,3 @@
-Import-Module BitsTransfer
-$Source = "\\vmware-host\Shared Folders\-vagrant\blobs\sqlserver_2012_ee_sp3.iso"
 $Destination = "C:\sqlserver_2012_ee_sp3.iso"
-Start-BitsTransfer -Source $Source -Destination $Destination -Description "Backup" -DisplayName "Backup"
 Mount-DiskImage $Destination
-Copy-Item "\\vmware-host\Shared Folders\-vagrant\http\windows-2012r2\ConfigurationFile.ini" "C:\ConfigurationFile.ini"
 & cmd.exe /c "E:\setup.exe /ConfigurationFile=c:\ConfigurationFile.ini /QS"
