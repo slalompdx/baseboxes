5fe276571666d8d71218317a6ba4d2cf02236b34
Eric Shamow <alice@example.com>
2016-10-03 10:40:43 -0700

    First pass for generalized rhn

---
+++ scripts/rhel/7/satellite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "${RHN_UNAME}" ] || [ -z "${RHN_PW}" ]
+then
+  echo "You must set RHN_UNAME and RHN_PW to register with RHN."
+  exit 1
+fi
+
+subscription-manager register --username $RHN_UNAME --password $RHN_PW
+subscription-manager attach
+yum -y upgrade
