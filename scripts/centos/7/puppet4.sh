086a31f200cb70a78445beda44590cfdb57a7636
Eric Shamow <alice@example.com>
2016-10-19 17:32:46 -0700

    Parameterize puppet version

--- scripts/centos/7/puppet4.sh
+++ scripts/centos/7/puppet4.sh
@@ -2,6 +2,13 @@
 
 yum clean all
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum -y install puppet-agent
+if [ -n "${PUPPET_VERSION}" ];
+then
+  echo "Installing agent version ${PUPPET_VERSION}"
+  yum -y install "puppet-agent-${PUPPET_VERSION}"
+else
+  echo "Installing latest agent version"
+  yum -y install puppet-agent
+fi
 echo 'PATH=$PATH:/opt/puppetlabs/bin/' >> /etc/profile
 ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
