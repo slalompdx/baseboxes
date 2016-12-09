643dece514083c65e64afd583f94950dc2bc2c29
Terri Haber <terri.haber@standard.com>
2016-12-09 13:22:20 -0800

    adding ruby layer, not finished

---
+++ spec/acceptance/ruby/base_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+$orig_packages =
+  [
+    'rpmdevtools',
+    'glibc-devel',
+    'readline-devel',
+    'libyaml-devel',
+    'ncurses-devel',
+    'gdbm-devel',
+    'tcl-devel',
+    'openssl-devel',
+    'libffi-devel',
+    'make',
+    'gcc',
+    'unzip',
+    'byacc'
+  ]
